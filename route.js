08ceaa765581964a9692d91c359b9a0f8cdedaa7
Bidipta-BG <gogoibidipta@gmail.com>
2022-11-13 21:35:39 +0530

    completed assignment

--- route.js
+++ route.js
@@ -10,19 +10,27 @@ const { authenticate } = require('@google-cloud/local-auth');
 const { google } = require('googleapis');
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 
-const credentials = {
-        client_id: "206717800097-pusbvi353vvlnh6idubl6l3rnv3lr07b.apps.googleusercontent.com",
-        project_id: "youthindia-assignment",
-        auth_uri: "https://accounts.google.com/o/oauth2/auth",
-        token_uri: "https://oauth2.googleapis.com/token", 
-        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        client_secret: "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", 
-        javascript_origins: ["http://localhost:3000"], 
-        redirect_uris: ["http://localhost:3000/gettingtoken"]
-    }
+
+// /**
+//  * Reads previously authorized credentials from the save file.
+//  *
+//  * @return {Promise<OAuth2Client|null>}
+//  */
+
+// /**
+//  * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
+//  *
+//  * @param {OAuth2Client} client
+//  * @return {Promise<void>}
+//  */
+
+// /**
+//  * Lists the next 10 events on the user's primary calendar.
+//  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+//  */
+
 
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json')
-
 
 
 router.post("/login", async function(req, res){
@@ -34,11 +42,12 @@ router.post("/login", async function(req, res){
         if(getToken[0].isLogIn == true) return res.send('User Already logged in. Please get the event from get event api or logout first')
     }
     if (getToken.length==0 || getToken[0].isLogIn == false){
-        console.log('working');
+        // console.log('working');
         let client = await authenticate({
             scopes: SCOPES,
             keyfilePath: CREDENTIALS_PATH,
         })
+        console.log(client);
         if(client.credentials){
             const content = await fs.readFile(CREDENTIALS_PATH);
             const keys = JSON.parse(content);
@@ -58,7 +67,7 @@ router.post("/login", async function(req, res){
                 return res.send({ message: 'logged in and saved token', token: saveData})
             }else{
                 let updateToken = await tokenModel.findOneAndUpdate({ type: 'authorized_user' }, payload, {new:true})
-                return res.send({ message: 'updated new token', token: updateToken })
+                return res.send({ message: 'loggendin and token updated', token: updateToken })
             }
         }
     }
@@ -76,40 +85,73 @@ router.get("/getevent", async function (req, res) {
 
     let credentials = { type , client_id, client_secret, refresh_token }
 
-    console.log(credentials)
-    let client = await google.auth.fromJSON(credentials)
+    // console.log(credentials)
     
-    console.log('auth= ', client)
-    
-    const calendar = await google.calendar({ version: 'v3', client });
-    
+    // console.log('client=', client)
     // return res.send('hi')
     
-    const resp = await calendar.events.list({
-        calendarId: 'primary',
-        timeMin: new Date().toISOString(),
-        maxResults: 10,
-        singleEvents: true,
-        orderBy: 'startTime',
-    });
 
-    console.log("object=calen= ", resp);
-    return res.send('hi')
-
-    const events = await resp.data.items;
-    if (!events || events.length === 0) {
-        console.log('No upcoming events found.');
-        return;
+    async function loadSaveTokenIfExists() {
+        try {
+            // console.log("credentials= ", credentials);
+            return google.auth.fromJSON(credentials);
+        } catch (err) {
+            return null;
+        }
     }
-    console.log('Upcoming 10 events:');
-    events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-    });
-    return res.send('hi')
+
+
+    async function authorize() {
+        let client = await loadSaveTokenIfExists();
+        // console.log('client=',client);
+        if (client) {
+            return client;
+        }
+    }
+    async function listEvents(auth) {   //this auth is client same
+        // console.log('auth= ', auth)
+        const calendar = google.calendar({ version: 'v3', auth });
+        const resp = await calendar.events.list({
+            calendarId: 'primary',
+            timeMin: new Date().toISOString(),
+            maxResults: 10,
+            singleEvents: true,
+            orderBy: 'startTime',
+        });
+        // console.log("object=calen= ", resp);
+        const events = resp.data.items;
+        if (!events || events.length === 0) {
+            // console.log('No upcoming events found.');
+            // outputEvent += 'No upcoming events found.'
+            return 'No upcoming events found.'
+            // return res.send({ message: 'No upcoming events found.'})
+        }
+        let upcomingEvent = []
+        // console.log('Upcoming 10 events:');
+        events.map((event, i) => {
+            const start = event.start.dateTime || event.start.date;
+            // console.log(event);
+            upcomingEvent.push({eventTime: start, eventTitle: event.summary,  eventDetails: event.description})
+        });
+        return upcomingEvent
+    }
+ 
+    let output =await authorize().then(listEvents)
+        .then((res) => res)
+    .catch(console.error);
+
+    console.log(output)
+    return res.send({'Your Upcoming Events': output})
 
 })
 
 
+
+
+
+router.post("/logout", async function (req, res) {
+    await tokenModel.findOneAndUpdate({ type: 'authorized_user' }, { isLogIn : false}, { new: true })
+    return res.status(200).send({ message: 'User Logget out Succeffully' })
+})
 
 module.exports = router
